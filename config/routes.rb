205df1df526e0238f539ccb0e9fb906ca2fa6335
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-21 02:03:01 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
+  end
 end
